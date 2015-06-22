5dac53e32ed496fa5a91c8936a69541e9b775093
Keijiro Takahashi <keijiro@ZX.local>
2015-06-23 00:03:12 +0900

    Added OscJackWindow

--- Assets/OscJack/OscDirectory.cs
+++ Assets/OscJack/OscDirectory.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using System;
 using System.Collections.Generic;
 
@@ -19,13 +20,18 @@ namespace OscJack
 
         #endregion
 
-        OscServer _server;
-        Dictionary<string, OscMessage> _dataMap;
+        #region Public Methods
 
         public OscDirectory()
         {
             _server = new OscServer();
             _dataMap = new Dictionary<string, OscMessage>();
+
+            #if UNITY_EDITOR
+            _messageHistory = new Queue<OscMessage>();
+            #endif
+
+            _server.Start();
         }
 
         public void StartServer()
@@ -38,20 +44,103 @@ namespace OscJack
             _server.Close();
         }
 
-        public void Update()
+        public System.Object[] GetData(string path)
         {
-            while (_server.MessageCount > 0)
+            UpdateIfNeeded();
+            OscMessage message;
+            _dataMap.TryGetValue(path, out message);
+            return message.data;
+        }
+
+        #endregion
+
+        #region Private Objects And Functions
+
+        // Server object and path-to-data map
+        OscServer _server;
+        Dictionary<string, OscMessage> _dataMap;
+
+        // Last update frame number
+        int _lastFrame;
+
+        void UpdateIfNeeded()
+        {
+            if (Application.isPlaying)
             {
-                var msg = _server.PopMessage();
-                _dataMap[msg.path] = msg;
+                var frame = Time.frameCount;
+                if (frame != _lastFrame) {
+                    UpdateState();
+                    _lastFrame = frame;
+                }
+            }
+            else
+            {
+                #if UNITY_EDITOR
+                if (CheckUpdateInterval()) UpdateState();
+                #endif
             }
         }
 
-        public Object[] GetData(string path)
+        void UpdateState()
         {
-            OscMessage msg;
-            _dataMap.TryGetValue(path, out msg);
-            return msg.data;
+            while (_server.MessageCount > 0)
+            {
+                var message = _server.PopMessage();
+                _dataMap[message.path] = message;
+
+                #if UNITY_EDITOR
+                // Record the message.
+                _totalMessageCount++;
+                _messageHistory.Enqueue(message);
+                #endif
+            }
+
+            #if UNITY_EDITOR
+            // Truncate the history.
+            while (_messageHistory.Count > 8)
+                _messageHistory.Dequeue();
+            #endif
         }
+
+        #endregion
+
+        #region Editor Support
+
+        #if UNITY_EDITOR
+
+        // Update timer
+        const float _updateInterval = 1.0f / 10;
+        float _lastUpdateTime;
+
+        bool CheckUpdateInterval()
+        {
+            var current = Time.realtimeSinceStartup;
+            if (current - _lastUpdateTime > _updateInterval || current < _lastUpdateTime) {
+                _lastUpdateTime = current;
+                return true;
+            }
+            return false;
+        }
+
+        // Total message count
+        int _totalMessageCount;
+
+        public int TotalMessageCount {
+            get {
+                UpdateIfNeeded();
+                return _totalMessageCount;
+            }
+        }
+
+        // Message history
+        Queue<OscMessage> _messageHistory;
+
+        public Queue<OscMessage> History {
+            get { return _messageHistory; }
+        }
+
+        #endif
+
+        #endregion
     }
 }
--- Assets/Tester.cs
+++ Assets/Tester.cs
@@ -3,15 +3,8 @@ using OscJack;
 
 public class Tester : MonoBehaviour
 {
-    void Start()
-    {
-        OscDirectory.Instance.StartServer();
-    }
-
     void Update()
     {
-        OscDirectory.Instance.Update();
-
         var data = OscDirectory.Instance.GetData("/audio/loud");
         if (data != null) Debug.Log(data[0]);
     }
